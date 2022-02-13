57225d818b763377623b1d5d2fd01a2b45eacc91
Bardi Harborow <bardi@bardiharborow.com>
2022-02-13 17:56:55 +1100

    Update Google Analytics

--- js/upriver.js
+++ js/upriver.js
@@ -132,9 +132,12 @@ $(function() {
   }
 });
 
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-ga('create', 'UA-96010261-3', 'auto');
-ga('send', 'pageview');
+/* Google Analytics */
+window.dataLayer = window.dataLayer || [];
+
+function gtag() {
+  dataLayer.push(arguments);
+}
+
+gtag('js', new Date());
+gtag('config', 'G-Y9H50SGVVY');
